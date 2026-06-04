2cb0ebbc8a3caa529df55777878f1fffd9f4561f
StrongFishWine <souzab50507@masseyhigh.school.nz>
2026-06-04 12:14:30 +1200

    trying

---
+++ square.py
@@ -0,0 +1,32 @@
+import math
+def square(side):
+    return {"area": side**2, "perimeter": side*4,}
+    side = num_check("side length: ")
+    print(f"Circle area: {square(side)['area']}")
+    print(f"Circle circumference: {square(side)['perimeter']}")
+    
+def num_check(question, num_type="float"):
+
+    if num_type == "float":
+        error = "Please enter a number."
+        zero = "Please enter a number more than 0."
+
+    while True:
+
+        response = input(question)
+
+        try:
+            if num_type == "float":
+                response = float(response)
+            else:
+                response = int(response)
+
+            if response > 0:
+                return response
+            else:
+                print(zero)
+
+        except ValueError:
+            print(error)
+
+
